a4636029ded8db8f8320918b073188d0942d0286
Bine Lipuš <bine.lipus114@gmail.com>
2021-01-20 11:57:00 +0100

    Speedup -> Check for division for odd numbers only

--- Java/src/main.java
+++ Java/src/main.java
@@ -4,75 +4,84 @@ import java.util.List;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
-        String last_prime_number = "0";
-        String a = "";
-        int step = 10000;
+        try {
 
-        File f = new File("prime_numbers.txt");
-        if (f.exists() && !f.isDirectory()) {
+            long start = System.currentTimeMillis();
+            String last_prime_number = "0";
+            String a = "";
+            int step = 10000;
 
-            FileReader fr = new FileReader("prime_numbers.txt");
-            BufferedReader br = new BufferedReader(fr);
-            while ((a = br.readLine()) != null) {
-                last_prime_number = a;
+            File f = new File("prime_numbers.txt");
+            if (f.exists() && !f.isDirectory()) {
+
+                FileReader fr = new FileReader("prime_numbers.txt");
+                BufferedReader br = new BufferedReader(fr);
+                while ((a = br.readLine()) != null) {
+                    last_prime_number = a;
+                }
+                br.close();
+                fr.close();
+
             }
-            br.close();
-            fr.close();
 
-        }
-
-        while (true)
-        {
-            List<String> prime_numbers = new ArrayList<String>();
-            int i = Integer.parseInt(last_prime_number); // Last prime number found in file
-            int k = ((i / step) * step);
-            if (i % step != 0)
-            {
-                k += step;
-            }
-            i = k;
-            int compute_to_num = i + step;
-            if (i==0) i = 2;
-            for (; i < compute_to_num; i++) {
-                int number_of_dividers = 0;
-                int sqrt = (int) Math.sqrt(i);
-                for (int j = 1; j <= sqrt; j++) {
-                    if (i % j == 0) {
-                        number_of_dividers++;
+            while (true) {
+                List<String> prime_numbers = new ArrayList<String>();
+                int i = Integer.parseInt(last_prime_number); // Last prime number found in file
+                int k = ((i / step) * step);
+                if (i % step != 0) {
+                    k += step;
+                }
+                i = k;
+                int compute_to_num = i + step;
+                if (i == 0) i = 2;
+                for (; i < compute_to_num; i++) {
+                    int number_of_dividers = 0;
+                    if (i%2 == 0 && i != 2) continue; // If the number is even, we know that it can be divided with 1
+                    // and 2, thus it is not a prime number except if it is a number 2.
+                    int sqrt = (int) Math.sqrt(i);
+                    for (int j = 1; j <= sqrt; j += 2) {
+                        // We can increment divider by 2, since none of prime numbers can be divided with even number,
+                        // except prime number 2.
+                        if (i % j == 0) {
+                            number_of_dividers++;
+                        }
+                        if (number_of_dividers > 1) {
+                            break;
+                        }
                     }
-                    if (number_of_dividers > 1) {
+                    if (number_of_dividers == 1) {
+                        prime_numbers.add(Integer.toString(i));
+                    }
+                }
+
+                long end = System.currentTimeMillis();
+                FileWriter fw = new FileWriter("log.txt", true); // True is for appending
+                fw.write("Search for prime numbers from " + (compute_to_num - step) + " to " + compute_to_num + " completed in " + (end - start) + " milliseconds.");
+                fw.write(" Found " + prime_numbers.size() + " prime numbers.\n");
+                fw.close();
+                start = System.currentTimeMillis();
+
+                while (true) {
+                    try {
+                        fw = new FileWriter("prime_numbers.txt", true); // True je za appendat.
                         break;
+                    } catch (Exception ex) {
                     }
                 }
-                if (number_of_dividers == 1) {
-                    prime_numbers.add(Integer.toString(i));
+
+                for (String prime_number : prime_numbers) {
+                    fw.write(prime_number + "\n");
+                    last_prime_number = prime_number;
                 }
+                fw.close();
+                prime_numbers.clear();
             }
-
-            long end = System.currentTimeMillis();
-            FileWriter fw = new FileWriter("log.txt", true); // True is for appending
-            fw.write("Search for prime numbers from " + (compute_to_num - step) + " to " + compute_to_num + " completed in " + (end - start) + " milliseconds.");
-            fw.write(" Found " + prime_numbers.size() + " prime numbers.\n");
+        }
+        catch (Exception ex)
+        {
+            FileWriter fw = new FileWriter("error.txt", true);
+            fw.write("Error: " + ex.toString());
             fw.close();
-            start = System.currentTimeMillis();
-
-            while (true)
-            {
-                try
-                {
-                    fw = new FileWriter("prime_numbers.txt", true); // True je za appendat.
-                    break;
-                }
-                catch (Exception ex) {}
-            }
-
-            for (String prime_number : prime_numbers) {
-                fw.write(prime_number + "\n");
-                last_prime_number = prime_number;
-            }
-            fw.close();
-            prime_numbers.clear();
         }
     }
 }
--- Python/graph.py
+++ Python/graph.py
@@ -3,18 +3,33 @@ import time as t
 file_name = "../Java/log.txt"
 
 start = t.time()
-
+data = []
 for line in open(file_name):
     line = line.strip().split()
-    range_start = int(line[5])
-    range_end = int(line[7])
-    time = int(line[10])
-    number_of_prime_numbers = int(line[13])
-    print(f"{range_start}-{range_end}, {time}, {number_of_prime_numbers}")
+    data.append([int(line[5]), int(line[7]), int(line[10]), int(line[13])])
+    print(data[-1])
+
 
 end = t.time()
 
 print(f"Finished in {(end-start):.2f} seconds")
+
+max_time = max([x[2] for x in data])
+stepX = (data[-1][1] // 100000) // risar.maxX
+stepY = risar.maxY / max_time
+
+last_y = risar.maxY
+for x in range(risar.maxX):
+    try:
+        risar.crta(x - 1, last_y, x - 1, risar.maxY - data[x*stepX][2] * stepY, risar.rdeca)
+        last_y = risar.maxY - data[x*stepX][2]
+    except Exception:
+        raise Exception
+        a = 1
+
+risar.stoj()
+
+
 
 
 
